4ee61f0124e6564936f208a765da651c23c1fefa
Fajri135 <corex135@gmail.com>
2025-10-17 10:32:34 +0700

    - impor Box, sebuah composable tata letak dasar yang berfungsi seperti sebuah wadah
    - modifier yang memberitahu sebuah composable untuk mengisi semua ruang
    
    - modifier yang digunakan untuk menambahkan ruang kosong di sekeliling bagian luar sebuah composab
    
    -Mengimpor Scaffold, sebuah komponen tata letak tingkat tinggi dari Material Design 3

--- app/src/main/java/com/example/t3/MainActivity.kt
+++ app/src/main/java/com/example/t3/MainActivity.kt
@@ -3,3 +3,7 @@ package com.example.t3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
